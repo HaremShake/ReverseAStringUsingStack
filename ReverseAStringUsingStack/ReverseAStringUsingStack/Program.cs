8730a5f66abc5b89075b854f11870a922adeab08
Pramkub <0x175208@protonmail.com>
2020-12-16 18:57:23 +0700

    Example of Reverse A String Using Stack

---
+++ ReverseAStringUsingStack/ReverseAStringUsingStack/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReverseAStringUsingStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "ReverseAStringUsingStack";
+            Console.WriteLine("Enter a String:");
+            string Input = Console.ReadLine();
+            string Output = ReverseAString(Input);
+
+            Console.WriteLine("\nReversed String is: " + Output);
+            Console.Read();  
+        }
+        private static string ReverseAString(string Input)
+        {
+            var objStack = new Stack<char>();
+            var OutPut = string.Empty;
+            if (Input != null)
+            {
+               var InputLength = Input.Length;
+
+               for (int i=0;i<InputLength;i++)  
+                {  
+                    objStack.Push(Input[i]);  
+                }  
+                
+                while (objStack.Count != 0)  
+                {  
+                    OutPut += objStack.Pop();  
+                }  
+            }  
+            return OutPut;  
+        }
+     }
+ }
+
